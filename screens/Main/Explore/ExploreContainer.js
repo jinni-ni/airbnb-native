03fb384b4b933a30d64a82cb1e888887813f6cf6
김수진 <sjkim910115@gmail.com>
2021-03-01 21:30:51 +0900

    explore room

--- screens/Main/Explore/ExploreContainer.js
+++ screens/Main/Explore/ExploreContainer.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ExplorePresenter from "./ExplorePresenter";
 
 export default ({ getRooms, rooms, page, increasePage }) => {
+  useEffect(() => {
+    getRooms(1);
+  }, []);
   useEffect(() => {
     getRooms(page);
   }, [page]);
